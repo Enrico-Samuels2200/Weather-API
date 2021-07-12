61b3a633ca1192c6079939a460f87f89f1cd5868
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2021-07-12 23:15:20 +0200

    Upgrade function to ES6.

--- routes/return.js
+++ routes/return.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 
 // Send email to database
 router.get('/', async (req, res) => {
-    mongoose.connection.db.collection("portfolio_projects", function (err, collection) {
-        collection.find({}).toArray(function (err, data) {
+
+    mongoose.connection.db.collection("portfolio_projects", (err, collection) => {
+        collection.find({}).toArray((err, data) => {
             if (err) {
                 res.send(err);
             } else {
